5e3f9ad55a14fa3c833d1e1d0082f5fb20c82bd2
Drew Bolles <andrew.bolles@ncr.com>
2018-10-30 12:50:04 -0400

    Add padding prop to box

--- src/Box.js
+++ src/Box.js
@@ -3,17 +3,30 @@ import classNames from 'classnames';
 
 import styles from './Box.css';
 
-const Box = ({ children, className, flex, gradient, bgColor, ...rest }) => (
+const Box = ({
+  children,
+  className,
+  flex,
+  gradient,
+  padding,
+  bgColor,
+  ...rest
+}) => (
   <div
     className={classNames('box', styles.className, className, {
       [`box--gradient-${gradient}`]: gradient,
       [`box--bg-${bgColor}`]: bgColor,
       'box--flex': flex,
     })}
+    style={{ padding }}
     {...rest}
   >
     <div className="box-inner">{children}</div>
   </div>
 );
+
+Box.defaultProps = {
+  padding: 16,
+};
 
 export default Box;
