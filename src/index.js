8a14c474f9afde6f89a0923e1b2d5786a4ea8ba2
Drew Bolles <andrew.bolles@ncr.com>
2018-10-30 12:39:32 -0400

    Export logo from index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Box } from './Box';
 export { default as Button } from './Button';
+export { default as Logo } from './Logo';
 export { default as Typography } from './Typography';
