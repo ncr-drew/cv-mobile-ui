7674ea21f928a0708f32743f78c5d3b80be573ab
Drew Bolles <andrew.bolles@ncr.com>
2018-10-30 16:16:29 -0400

    remove logo component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export { default as Box } from './Box';
 export { default as Button } from './Button';
-export { default as Logo } from './Logo';
 export { default as PointsMeter } from './PointsMeter';
 export { default as Typography } from './Typography';
